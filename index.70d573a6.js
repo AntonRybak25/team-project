4a919138f0686c213deba0ac9673e52c9ccb2c9a
AntonRybak25 <107934664+AntonRybak25@users.noreply.github.com>
2022-08-31 09:01:13 +0000

    Deploying to gh-pages from @ AntonRybak25/team-project@e461a72a498b84b8ac856e0df30c20d9f9365351 🚀

--- index.70d573a6.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e=document.querySelector("[data-menu-open]"),t=document.querySelector("[data-menu-close]"),n=document.querySelector("[data-menu]");function c(){n.classList.toggle("is-open")}e.addEventListener("click",c),t.addEventListener("click",c)})();
-//# sourceMappingURL=index.70d573a6.js.map
