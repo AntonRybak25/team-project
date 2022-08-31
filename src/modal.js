2493f78daccdb8b3a8ecbf5031e78abf3b8237c0
AntonRybak25 <antonbest99@gmail.com>
2022-08-31 21:14:05 +0300

    update modal

--- src/modal.js
+++ src/modal.js
@@ -1,8 +1,16 @@
 
-(() => { const refs = { openModalBtn:
-document.querySelector("[data-modal-open]"), closeModalBtn:
-document.querySelector("[data-modal-close]"), modal:
-document.querySelector("[data-modal]"), };
-refs.openModalBtn.addEventListener("click", toggleModal);
-refs.closeModalBtn.addEventListener("click", toggleModal); function
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector("[data-modal-open]"),
+        openModalBtn1: document.querySelector("[data-modal-open-1]"),
+        openModalBtn2: document.querySelector("[data-modal-open-2]"),
+    closeModalBtn:
+        document.querySelector("[data-modal-close]"), modal:
+        document.querySelector("[data-modal]"),
+    };
+    
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.openModalBtn1.addEventListener("click", toggleModal);
+    refs.openModalBtn2.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal); function
 toggleModal() { refs.modal.classList.toggle("is-hidden"); } })();
