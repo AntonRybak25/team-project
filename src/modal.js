65a2056596894e68891b9a59ddaa91a0f33e9252
AntonRybak25 <antonbest99@gmail.com>
2022-08-31 11:53:24 +0300

    Update modal.js

--- src/modal.js
+++ src/modal.js
@@ -1,4 +1,4 @@
-/*
+
 (() => { const refs = { openModalBtn:
 document.querySelector("[data-modal-open]"), closeModalBtn:
 document.querySelector("[data-modal-close]"), modal:
@@ -6,20 +6,3 @@ document.querySelector("[data-modal]"), };
 refs.openModalBtn.addEventListener("click", toggleModal);
 refs.closeModalBtn.addEventListener("click", toggleModal); function
 toggleModal() { refs.modal.classList.toggle("is-hidden"); } })();
-*/
-
-(() => {
-    const menuOpenBtn = document.querySelector('[data-menu-open]');
-    const menuCloseBtn = document.querySelector('[data-menu-close]');
-  
-    const mobileMenu = document.querySelector('[data-menu]');
-    // const body = document.querySelector('body');
-  
-    menuOpenBtn.addEventListener('click', openModal);
-    menuCloseBtn.addEventListener('click', openModal);
-  
-    function openModal() {
-      mobileMenu.classList.toggle('is-open');
-      // body.classList.toggle('no-scroll');
-    }
-  })();
