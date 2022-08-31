f9c3c0c1dc67f8688b69d23bcadcabacfd6a65e1
AntonRybak25 <107934664+AntonRybak25@users.noreply.github.com>
2022-08-31 18:16:17 +0000

    Deploying to gh-pages from @ AntonRybak25/team-project@28ca3cdd1b48641e8485b73d01f61a6ef609656d 🚀

---
+++ index.44466fb5.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),openModalBtn1:document.querySelector("[data-modal-open-1]"),openModalBtn2:document.querySelector("[data-modal-open-2]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.openModalBtn1.addEventListener("click",o),e.openModalBtn2.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.44466fb5.js.map
